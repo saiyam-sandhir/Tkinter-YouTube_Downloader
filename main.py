21e3bbf87f5280eec331753bb8896ee24b4c1e48
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-02-13 23:51:18 +0530

    Add thumbnail labelframe to the layout
    resolve #3

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ class YouTubeDownloader(tk.Tk):
         self.iconbitmap(".\\icon.ico")#credit: flaticon.com
         self.config(padx=20, pady=20)
 
-        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(2, weight=1)
 
         #----------Load Video/Playlist----------#
         tk.Label(self, text="Video/Playlist URL:").grid(row=0, column=0, sticky=tk.W)
 
         url_entry = tk.Entry(self)
-        url_entry.grid(row=1, column=0, sticky = tk.EW)
+        url_entry.grid(row=1, column=0, columnspan = 2, sticky = tk.EW)
 
         load_button = tk.Button(self, text="Load")
-        load_button.grid(row=1, column=1, sticky=tk.W)
+        load_button.grid(row=1, column=2, sticky=tk.W)
+
+        #----------Thumbnail----------#
+        thumbnail_labelframe = tk.LabelFrame(self, text="Thumbnail", width=400, height=225)
+        thumbnail_labelframe.grid(row=2, column=1)
+        thumbnail_labelframe.grid_rowconfigure(0, weight=1)
+        thumbnail_labelframe.grid_columnconfigure(0, weight=1)
 
         self.mainloop()
         
