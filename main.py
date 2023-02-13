fbcc0ed4f51a17dc72e8f4d4ac75bd40bb2e42c5
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-02-13 22:15:57 +0530

    Add Entry box and load button to the layout
    resolve #2

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ class YouTubeDownloader(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Tkinter-YouTube_Downloader")
-        self.iconbitmap(".\\icon.ico")
+        self.iconbitmap(".\\icon.ico")#credit: flaticon.com
+        self.config(padx=20, pady=20)
+
+        self.columnconfigure(0, weight = 1)
+
+        #----------Load Video/Playlist----------#
+        tk.Label(self, text="Video/Playlist URL:").grid(row=0, column=0, sticky=tk.W)
+
+        url_entry = tk.Entry(self)
+        url_entry.grid(row=1, column=0, sticky = tk.EW)
+
+        load_button = tk.Button(self, text="Load")
+        load_button.grid(row=1, column=1, sticky=tk.W)
 
         self.mainloop()
         
